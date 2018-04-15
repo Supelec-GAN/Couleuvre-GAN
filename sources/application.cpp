c6c33fb57994118982affbbb54a6cade59171e89
Benjamin Mouscadet <benjamin.mouscadet@supelec.fr>
2018-04-15 15:05:34 +0000

    Ajout dans la methode Application::Application d'exemples d'utilisation des flags bitwise pour CifarLabel

--- sources/application.cpp
+++ sources/application.cpp
@@ -18,7 +18,16 @@ Application::Application()
 
     try
     {
-        InputProvider::Ptr inputProvider(new Cifar10Provider(Cifar10Provider::CifarLabel::airplane, 10000, 10000));
+        Cifar10Provider::CifarLabel CifVehicle =   Cifar10Provider::CifarLabel::airplane | Cifar10Provider::CifarLabel::automobile |
+                                                Cifar10Provider::CifarLabel::ship | Cifar10Provider::CifarLabel::truck;
+
+        Cifar10Provider::CifarLabel CifAnimals =   Cifar10Provider::CifarLabel::bird | Cifar10Provider::CifarLabel::cat |
+                                                Cifar10Provider::CifarLabel::deer | Cifar10Provider::CifarLabel::dog |
+                                                Cifar10Provider::CifarLabel::horse | Cifar10Provider::CifarLabel::frog;
+
+        Cifar10Provider::CifarLabel CifAll = CifAnimals | CifVehicle;
+
+        InputProvider::Ptr inputProvider(new Cifar10Provider(CifAll, 10000, 10000));
 
         mTeachingBatchDis = inputProvider->trainingBatch();
         mTestingBatchDis = inputProvider->testingBatch();
